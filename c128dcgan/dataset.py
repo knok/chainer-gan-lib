75f893441d0878e41d457fb8857bf0251b06334c
NOKUBI Takatsugu <knok@daionet.gr.jp>
2017-08-25 14:46:24 +0900

    npz image loader/saver

--- c128dcgan/dataset.py
+++ c128dcgan/dataset.py
@@ -29,3 +29,47 @@ class Color128x128Dataset(chainer.dataset.DatasetMixin):
         img -= 128
         img /= 128
         return img
+
+class NPZColorDataset(chainer.dataset.DatasetMixin):
+    def __init__(self, data_dir = None, npz=None):
+        self.data_dir = data_dir
+        self.width = 128
+        self.height = 128
+
+        if npz is not None:
+            self.images = np.load(npz)
+            self.npz_fname = npz
+        else:
+            if data_dir is None:
+                raise Error("data_dir is None")
+            images = []
+            for fname in os.listdir(data_dir):
+                if fname.endswith(".png") or fname.endswith('.jpg'):
+                    fullpath_fname = os.path.join(data_dir, fname)
+                    img = np.asarray(read_image(fullpath_fname, color=True),
+                                     dtype=np.float32)
+                    img -= 128
+                    img /= 128
+                    images.append(img)
+                self.images = np.asarray(images)
+
+    def __len__(self):
+        return len(self.images)
+
+    def get_example(self, i):
+        if i > len(self):
+            raise IndexError('index is too large')
+        return self.images[i]
+
+    def save_images(self, fname):
+        np.savez(fname, self.images)                
+
+if __name__ == '__main__':
+    import argparse
+    p = argparse.ArgumentParser()
+    p.add_argument('--data-dir')
+    p.add_argument('output_fname', default='images.npz')
+    args = p.parse_args()
+    imgs = NPZColorDataset(args.data_dir)
+    imgs.save_images(args.output_fname)
+
