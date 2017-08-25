a7f577ee05824811e9a12d3c5f60d9bfabcbb1d9
NOKUBI Takatsugu <knok@daionet.gr.jp>
2017-08-25 10:06:55 +0900

    color 128x128 sndcgan

--- common/net.py
+++ common/net.py
@@ -388,3 +388,97 @@ class ResnetDiscriminator(chainer.Chain):
         g = self.r1.differentiable_backward(g)
         g = self.r0.differentiable_backward(g)
         return g
+
+# Color 128
+class C128Generator(chainer.Chain):
+    def __init__(self, n_hidden=128, bottom_width=4, ch=512, wscale=0.02,
+                 z_distribution="uniform", hidden_activation=F.relu, output_activation=F.tanh, use_bn=True):
+        super(C128Generator, self).__init__()
+        self.n_hidden = n_hidden
+        self.ch = ch
+        self.bottom_width = bottom_width
+        self.z_distribution = z_distribution
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
+        self.use_bn = use_bn
+
+        with self.init_scope():
+            w = chainer.initializers.Normal(wscale)
+            self.l0 = L.Linear(self.n_hidden, bottom_width * bottom_width * ch,
+                               initialW=w)
+            self.dc1 = L.Deconvolution2D(ch, ch // 2, 4, 2, 1, initialW=w)
+            self.dc2 = L.Deconvolution2D(ch // 2, ch // 4, 4, 2, 1, initialW=w)
+            self.dc3 = L.Deconvolution2D(ch // 4, ch // 8, 4, 2, 1, initialW=w)
+            self.dc4 = L.Deconvolution2D(ch // 8, ch // 16, 4, 2, 1, initialW=w)
+            self.dc5 = L.Deconvolution2D(ch // 16, ch // 32, 4, 2, 1, initialW=w)
+            self.dc6 = L.Deconvolution2D(ch // 32, 3, 3, 1, 1, initialW=w)
+            if self.use_bn:
+                self.bn0 = L.BatchNormalization(bottom_width * bottom_width * ch)
+                self.bn1 = L.BatchNormalization(ch // 2)
+                self.bn2 = L.BatchNormalization(ch // 4)
+                self.bn3 = L.BatchNormalization(ch // 8)
+                self.bn4 = L.BatchNormalization(ch // 16)
+                self.bn5 = L.BatchNormalization(ch // 32)
+
+    def make_hidden(self, batchsize):
+        if self.z_distribution == "normal":
+            return np.random.randn(batchsize, self.n_hidden, 1, 1) \
+                .astype(np.float32)
+        elif self.z_distribution == "uniform":
+            return np.random.uniform(-1, 1, (batchsize, self.n_hidden, 1, 1)) \
+                .astype(np.float32)
+        else:
+            raise Exception("unknown z distribution: %s" % self.z_distribution)
+
+    def __call__(self, z):
+        if not self.use_bn:
+            h = F.reshape(self.hidden_activation(self.l0(z)),
+                          (len(z), self.ch, self.bottom_width, self.bottom_width))
+            h = self.hidden_activation(self.dc1(h))
+            h = self.hidden_activation(self.dc2(h))
+            h = self.hidden_activation(self.dc3(h))
+            h = self.hidden_activation(self.dc4(h))
+            h = self.hidden_activation(self.dc5(h))
+            x = self.output_activation(self.dc6(h))
+        else:
+            h = F.reshape(self.hidden_activation(self.bn0(self.l0(z))),
+                          (len(z), self.ch, self.bottom_width, self.bottom_width))
+            h = self.hidden_activation(self.bn1(self.dc1(h)))
+            h = self.hidden_activation(self.bn2(self.dc2(h)))
+            h = self.hidden_activation(self.bn3(self.dc3(h)))
+            h = self.hidden_activation(self.bn3(self.dc4(h)))
+            h = self.hidden_activation(self.bn3(self.dc5(h)))
+            x = self.output_activation(self.dc6(h))
+        return x
+
+class SND128Discriminator(chainer.Chain):
+    def __init__(self, bottom_width=4, ch=512, wscale=0.02, output_dim=1):
+        w = chainer.initializers.Normal(wscale)
+        super(SND128Discriminator, self).__init__()
+        with self.init_scope():
+            self.c0_0 = SNConvolution2D(3, ch // 32, 3, 1, 1, initialW=w)
+            self.c0_1 = SNConvolution2D(ch // 32, ch // 16, 4, 2, 1, initialW=w)
+            self.c1_0 = SNConvolution2D(ch // 16, ch // 16, 3, 1, 1, initialW=w)
+            self.c1_1 = SNConvolution2D(ch // 16, ch // 8, 4, 2, 1, initialW=w)
+            self.c2_0 = SNConvolution2D(ch // 8, ch // 8, 3, 1, 1, initialW=w)
+            self.c2_1 = SNConvolution2D(ch // 8, ch // 4, 4, 2, 1, initialW=w)
+            self.c3_0 = SNConvolution2D(ch // 4, ch // 4, 3, 1, 1, initialW=w)
+            self.c3_1 = SNConvolution2D(ch // 4, ch // 2, 4, 2, 1, initialW=w)
+            self.c4_0 = SNConvolution2D(ch // 2, ch // 2, 3, 1, 1, initialW=w)
+            self.c4_1 = SNConvolution2D(ch // 2, ch // 1, 4, 2, 1, initialW=w)
+            self.c5_0 = SNConvolution2D(ch // 1, ch // 1, 3, 1, 1, initialW=w)
+            self.l6 = SNLinear(bottom_width * bottom_width * ch, output_dim, initialW=w)
+
+    def __call__(self, x):
+        h = F.leaky_relu(self.c0_0(x))
+        h = F.leaky_relu(self.c0_1(h))
+        h = F.leaky_relu(self.c1_0(h))
+        h = F.leaky_relu(self.c1_1(h))
+        h = F.leaky_relu(self.c2_0(h))
+        h = F.leaky_relu(self.c2_1(h))
+        h = F.leaky_relu(self.c3_0(h))
+        h = F.leaky_relu(self.c3_1(h))
+        h = F.leaky_relu(self.c4_0(h))
+        h = F.leaky_relu(self.c4_1(h))
+        h = F.leaky_relu(self.c5_0(h))
+        return self.l6(h)
